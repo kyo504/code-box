320cfcd422aa137c91818e1d6536828aeceaf568
Hunkyo Jung <hunkyo.jung@lge.com>
2016-04-15 17:50:26 +0900

    Implement simple-MVC

--- design-pattern/Simple-MVC/main.cpp
+++ design-pattern/Simple-MVC/main.cpp
@@ -1,5 +1,24 @@
 #include <stdio.h>
 #include <stdlib.h>
+#include <iostream>
+#include <vector>
+using namespace std;
+
+
+// http://cplusplusbench.blogspot.kr/2007/05/mvc-simple-implementation.html
+
+class IObserver
+{
+public:
+	virtual void Update() = 0;
+};
+
+class ISubject
+{
+public:
+	virtual void Register(IObserver*) = 0;
+	virtual void Unregister() = 0;
+};
 
 
 class Object
@@ -21,26 +40,94 @@ public:
 	}
 };
 
-class View
+class Controller;
+
+class View : public IObserver
 {
 public:
+	View(Controller *controller) {
+		mController = controller;
+		controller->Register(this);
+	}
 
+	void EnterAge() {
+		cout << "Enter Age";
+		int x = 0;
+		cin >> x;
+		mController->ModifyAge(x);
+	}
+
+	void Update() {
+		cout << "View UPdater Called ......";
+	}
+
+private:
+	Controller *mController;
 };
 
 class Controller
 {
 public:
+	Controller(ISubject* subject) {
+		mSubject = subject;
+	}
 
+	void ModifyAge(int newAge) {
+		dynamic_cast<>(mSubject)
+		(dynamic_cast(mSubject))->SetAge(newAge);
+	}
+
+	void Register(IObserver *viewobj) {
+		mSubject->Register(viewobj);
+	}
+
+private:
+	ISubject *mSubject;
 };
 
-class Model
+class Model : public ISubject
 {
 public:
+	virtual void Register(IObserver *observingObject) {
+		_observerMap.push_back(observingObject);
+	}
 
+	virtual void UnRegister() {
+		// TBD
+	}
+
+	int GetAge() {
+		return age;
+	}
+	void SetAge(int newAge) {
+		age = newAge;
+		NotifyObserver();
+	}
+
+private:
+	int age;
+	vector<IObserver*> _observerMap;
+	void NotifyObserver(){
+		for (int i = 0; i < static_cast<int>(_observerMap.size()); i++) {
+			_observerMap[i]->Update();
+		}
+	}
 };
 
 int main()
 {
+	/*
+	View* view = new View();
+	Controller* controller = new Controller();
+	Model* model = new Model();
+	/**/
+
+	Model *model = new Model();
+	Controller * controller = new Controller(model);
+	View *view = new View(controller);
+
+	view->EnterAge();
+
 
 
 	return 0;
